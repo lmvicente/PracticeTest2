9e5f56b54423716f35295a207cc2f90883608dd0
lmvicente <leah.m.vicente@gmail.com>
2018-11-29 14:35:41 -0800

    developed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface IContact {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  owed: number;
+  phone: string;
+}
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,7 +21,8 @@ import { ToastService } from '../toast/toast.service';
 })
 export class ContactComponent implements OnInit {
 
-  contacts: Array<any> = [];
+  contacts: Array<IContact> = [];
+  params: string;
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -23,7 +32,69 @@ export class ContactComponent implements OnInit {
   }
 
   async ngOnInit() {
-
+    this.contacts = await this.loadContacts();
   }
 
+  async loadContacts() {
+    let contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts && contacts.length > 0) {
+      // contacts = contacts;
+    } else {
+      contacts = await this.loadContactsFromJson();
+    }
+    console.log('this.contacts from ngOninit ... ', this.contacts);
+    this.contacts = contacts;
+    return contacts;
+  }
+
+  async loadContactsFromJson() {
+    const contacts = await this.http.get('assets/contacts.json').toPromise();
+    return contacts.json();
+  }
+
+  addContact() {
+    const contact: IContact = {
+      id: null,
+      firstName: null,
+      lastName: null,
+      email: null,
+      owed: null,
+      phone: null
+    };
+    this.contacts.unshift(contact);
+    this.saveLocalStorage();
+  }
+
+  deleteContact(index: number) {
+    this.contacts.splice(index, 1);
+    this.saveLocalStorage();
+  }
+
+  saveLocalStorage() {
+    localStorage.setItem('contacts', JSON.stringify(this.contacts));
+  }
+  finalize() {
+   const data =  this.calculate();
+    this.router.navigate(['home', data]);
+    }
+
+    search(params: string) {
+      this.contacts.filter((contact: IContact) => {
+        return contact.firstName.toLowerCase() === params.toLowerCase();
+      });
+    }
+  calculate() {
+    let owed = 0;
+    for (let i = 0; i < this.contacts.length; i++) {
+      // console.log(' i ====', i, 'this.contacts[i]', this.contacts[i]);
+      owed += this.contacts[i].owed;
+      // console.log('owed ====', owed);
+    }
+    return {
+      numberOfContacts: this.contacts.length,
+      subTotal: owed,
+      taxAmount: owed * 0.10,
+      total: owed + (owed * 0.10)
+    };
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../localStorageService';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { InnerSubscriber } from 'rxjs/internal/InnerSubscriber';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -12,8 +19,30 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router, private toastService: ToastService) {
   }
 
+  user: IUser = {
+    username: null,
+    password: null,
+  };
+
   ngOnInit() {
 
   }
+  login(user: IUser) {
+    // console.log(user);
 
+    const presetUser = { username: 'leah', password: 'leah123' };
+    if (user.username != null && user.password != null && user.username !== '' && user.password !== '') {
+      // log user in
+      // console.log('from within if statement..');
+      if (user.username === presetUser.username && user.password === presetUser.password) {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.router.navigate(['contacts', user]);
+      } else {
+        this.toastService.showToast('warning', 2000, 'Input correct credentials');
+      }
+    } else {
+      // console.log('specify credidential');
+      this.toastService.showToast('danger', 2000, 'Missing username or password.');
+    }
+  }
 }
